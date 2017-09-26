be2dfd020908f32d926db87e5e34a42cb4df041c
sergeyzin <sergeyzin1996@gmail.com>
2017-09-26 11:09:51 +0300

    Добавил рисование

--- assignment3/raster_algorithms/Form1.cs
+++ assignment3/raster_algorithms/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,59 @@ namespace raster_algorithms
 {
     public partial class Form1 : Form
     {
+        private Graphics g;
+        Point lastPoint = Point.Empty;
+        bool isMouseDown = false;
+
+
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = e.Location;
+            isMouseDown = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown == true)//check to see if the mouse button is down
+            {
+                if (lastPoint != null)//if our last point is not null, which in this case we have assigned above
+                {
+                    if (pictureBox1.Image == null)//if no available bitmap exists on the picturebox to draw on
+                    {
+                        //create a new bitmap
+                        Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                        pictureBox1.Image = bmp; //assign the picturebox.Image property to the bitmap created
+                    }
+
+                    using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+                    {
+                        g.DrawLine(new Pen(Color.Black, 2), lastPoint, e.Location);
+                        g.SmoothingMode = SmoothingMode.AntiAlias;
+                        
+
+                    }
+
+                    pictureBox1.Invalidate(); //refreshes the picturebox
+
+                    lastPoint = e.Location;//keep assigning the lastPoint to the current mouse position
+
+                }
+
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+            lastPoint = Point.Empty;
         }
     }
 }
