42be4dbb383015c58a9e6280f13c2f28fd73d54e
sergeyzin <sergeyzin1996@gmail.com>
2017-10-09 21:06:52 +0300

    Добавил рисование ребра

---
+++ assignment4/affine_transform/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace affine_transform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.edgeButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(770, 405);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // edgeButton
+            // 
+            this.edgeButton.BackgroundImage = global::affine_transform.Properties.Resources.line;
+            this.edgeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.edgeButton.Location = new System.Drawing.Point(6, 19);
+            this.edgeButton.Name = "edgeButton";
+            this.edgeButton.Size = new System.Drawing.Size(28, 29);
+            this.edgeButton.TabIndex = 1;
+            this.edgeButton.UseVisualStyleBackColor = true;
+            this.edgeButton.Click += new System.EventHandler(this.edgeButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.edgeButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(770, 60);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 495);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button edgeButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ assignment4/affine_transform/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace affine_transform
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        private Point startPoint, endPoint = Point.Empty;
+        private bool isMouseDown = false;
+        private bool edgeSelected = false;
+        private List<TwoPoints> points = new List<TwoPoints>();
+        private Pen penColor = Pens.LimeGreen;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+
+        }
+
+        private void edgeButton_Click(object sender, EventArgs e)
+        {
+            if (!edgeSelected)
+            {
+                pictureBox1.Cursor = Cursors.Cross;
+                edgeSelected = true;
+                edgeButton.BackColor = Color.LightBlue;
+            }
+            else
+            {
+                pictureBox1.Cursor = Cursors.Default;
+                edgeSelected = false;
+                edgeButton.BackColor = SystemColors.Control;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (edgeSelected)
+            {
+                startPoint = e.Location;
+                isMouseDown = true;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown && edgeSelected && startPoint != null)
+            {
+                endPoint = e.Location;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+            points.Add(new TwoPoints(startPoint, endPoint));
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (TwoPoints tp in points)
+            {
+                e.Graphics.DrawLine(penColor, tp.p1, tp.p2);
+            }
+            e.Graphics.DrawLine(penColor, startPoint, endPoint);
+        }
+    }
+
+}
