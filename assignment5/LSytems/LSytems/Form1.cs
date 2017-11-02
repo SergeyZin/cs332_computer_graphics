08bfb4b21edf6120e832777ca415c2d40bf1fe18
sergeyzin <sergeyzin1996@gmail.com>
2017-11-02 23:59:32 +0300

    The project was created

---
+++ assignment5/LSytems/LSytems/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LSytems
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        string axiom;
+        double angle;
+        string direction;
+        SortedDictionary<char, string> rules;
+        int iterations;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+        }
+    }
+}
